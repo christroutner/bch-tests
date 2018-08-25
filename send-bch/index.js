14b9507451b02a175e0d51653295b21b0613238d
Chris Troutner <chris.troutner@gmail.com>
2018-08-25 15:48:48 -0700

    Stuck on send-bch

--- send-bch/index.js
+++ send-bch/index.js
@@ -2,7 +2,7 @@
   Sends some BCH from one address to another.
 
   Uses hard-coded values and a pre-generated wallet that has been funded with
-  test BCH. 
+  test BCH.
 */
 
 "use strict";
@@ -10,35 +10,94 @@
 let BITBOXCli = require("bitbox-cli/lib/bitbox-cli").default;
 let BITBOX = new BITBOXCli({ restURL: "https://trest.bitcoin.com/v1/" });
 
-//let lang = langs[Math.floor(Math.random()*langs.length)];
-let lang = "english";
+const lib = require('../../token-liquidity/lib/util.js');
 
-// create 256 bit BIP39 mnemonic
-let mnemonic = BITBOX.Mnemonic.generate(256, BITBOX.Mnemonic.wordLists()[lang]);
-console.log("BIP44 $BCH Wallet");
-console.log(`256 bit ${lang} BIP39 Mnemonic: `, mnemonic);
+const SEND_MNEMONIC = `pact whisper custom vintage busy dolphin lumber pretty bottom motion alcohol innocent affair view bulb wheel rude load someone crazy hire trim surprise wing`;
+const SEND_ADDR = `bchtest:qzgjlerm2sfgpg562w2t6vm02tvdf3vpjvcea7nvrx`;
 
-// root seed buffer
-let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic);
+const RECV_ADDR = `bchtest:qzjwc0musf8vqp4nuau2t6fl099x6dgh2uspatyjvj`;
 
-// master HDNode
-let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed);
+async function sendBch() {
+  const balance = await lib.getBCHBalance(SEND_ADDR, false);
+  console.log(`Balance of sending address ${SEND_ADDR} is ${balance} BCH.`);
 
-// HDNode of BIP44 account
-let account = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'");
-console.log(`BIP44 Account: "m/44'/145'/0'"`);
+  if(balance <= 0.0) {
+    console.log(`Balance of sending address is zero. Exiting.`);
+    process.exit(0);
+  }
 
-for (let i = 0; i < 10; i++) {
-  let childNode = masterHDNode.derivePath(`m/44'/145'/0'/0/${i}`);
-  console.log(`m/44'/145'/0'/0/${i}: ${BITBOX.HDNode.toCashAddress(childNode)}`);
+  const balance2 = await lib.getBCHBalance(RECV_ADDR, false);
+  console.log(`Balance of recieving address ${RECV_ADDR} is ${balance2} BCH.`);
+
+  const utxo = await BITBOX.Address.utxo(SEND_ADDR);
+  console.log(`utxo: ${JSON.stringify(utxo,null,2)}`);
+
+  //process.exit(0);
+
+  // instance of transaction builder
+  let transactionBuilder = new BITBOX.TransactionBuilder("testnet");
+
+  const satoshisToSend = 1000;
+  let originalAmount = utxo[0].satoshis;
+  const vout = utxo[0].vout;
+  const txid = utxo[0].txid;
+
+  // add input with txid and index of vout
+  transactionBuilder.addInput(txid, vout);
+
+  // get byte count to calculate fee. paying 1 sat/byte
+  let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 2 });
+  const satoshisPerByte = 1.2;
+
+  // amount to send back to the sending address. It's the original amount - 1 sat/byte for tx size
+  let remainder = originalAmount - satoshisToSend - satoshisPerByte*byteCount;
+
+  // add output w/ address and amount to send
+  transactionBuilder.addOutput(RECV_ADDR, satoshisToSend);
+  transactionBuilder.addOutput(SEND_ADDR, remainder);
+
+  // Generate a change address from a Mnemonic of a private key.
+  const change = changeAddrFromMnemonic(SEND_MNEMONIC);
+
+  // Generate a keypair from the change address.
+  const keyPair = BITBOX.HDNode.toKeyPair(change);
+
+  // Sign the transaction with the HD node.
+  let redeemScript;
+  transactionBuilder.sign(0, keyPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, satoshisToSend);
+
+  // build tx
+  let tx = transactionBuilder.build();
+  // output rawhex
+  let hex = tx.toHex();
+  console.log(`Transaction raw hex: `);
+  console.log(`${hex}`);
+
+  // sendRawTransaction to running BCH node
+  const broadcast = await BITBOX.RawTransactions.sendRawTransaction(hex);
+  console.log(`Transaction ID: ${broadcast}`);
+
+}
+sendBch();
+
+// Generate a change address from a Mnemonic of a private key.
+function changeAddrFromMnemonic(mnemonic) {
+  // root seed buffer
+  let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic);
+
+  // master HDNode
+  let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed, 'testnet');
+
+  // HDNode of BIP44 account
+  let account = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'");
+
+  // derive the first external change address HDNode which is going to spend utxo
+  let change = BITBOX.HDNode.derivePath(account, "0/0");
+
+  return change;
 }
 
-// derive the first external change address HDNode which is going to spend utxo
-let change = BITBOX.HDNode.derivePath(account, "0/0");
-
-// get the cash address
-let cashAddress = BITBOX.HDNode.toCashAddress(change);
-
+/*
 let hex;
 
 BITBOX.Address.utxo(cashAddress).then(
@@ -103,3 +162,4 @@ BITBOX.Address.utxo(cashAddress).then(
     console.log(err);
   }
 );
+*/
