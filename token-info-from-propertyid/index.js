0514ea37fba8abbcc29ed1e1cfac58fa96c30402
Chris Troutner <chris.troutner@gmail.com>
2018-08-25 09:05:11 -0700

    Reduced token-info-from-propertyid

--- token-info-from-propertyid/index.js
+++ token-info-from-propertyid/index.js
@@ -1,17 +1,9 @@
-const config = require("./config.js");
-
-let BITBOXCli = require("bitbox-cli/lib/bitbox-cli").default;
-let BITBOX = new BITBOXCli(config);
-const fs = require("fs");
+const config = require("../config.js");
 
 let Wormhole = require("wormholecash/lib/Wormhole").default;
 let wormhole = new Wormhole(config);
 
-const rp = require("request-promise");
-
-const BCH_ADDR1 = `bchtest:qq26vgef48hg26e666qtkvthjsmt4z6phyw5mgltlr`;
-const TXID = `86c868c8e312d849d1cf1b10236353ed3513cc669c761d1c5a208d6f5c3cfb84`;
-const propertyId = 182;
+const propertyId = 186;
 
 async function getTokenInfo() {
   const retVal = await wormhole.DataRetrieval.property(propertyId);
